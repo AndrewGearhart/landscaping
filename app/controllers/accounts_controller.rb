f4dbfa31493fd9d00e95013186751ec884be3117
Alex Kiessling <ajkiessl@gmail.com>
2019-11-22 16:35:50 -0500

    Accounts api set up

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,5 +1,29 @@
 class AccountsController < ApplicationController
-  def get
+  skip_before_action :verify_authenticity_token
 
+  def get
+    @accounts = Account.all
+    render json: @accounts
+  end
+
+  def get_one
+    @account = Account.find_by(name: params[:name])
+    return render json: @account if @account
+
+    render json: { error: 'ACCOUNTNOTFOUND' }
+  end
+
+  def create_account
+    #TODO Validations
+    begin
+      data = JSON.parse("#{request.body.read}")
+      name = data['name']
+      account = Account.create(name: name) if name
+      return render json: account if account
+
+      render json: { error: 'ACCOUNTNOTCREATED' }
+    rescue JSON::ParserError => e
+      render json: { error: e.to_s }
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/accounts/', to: 'accounts#get'
+  get '/accounts/:name', to: 'accounts#get_one'
+
+  post '/accounts/', to: 'accounts#create_account'
 end
