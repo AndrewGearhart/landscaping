985ab04b64abf0cec5d5bb5804a80728581c4107
Dann Bohn <djb44@psu.edu>
2019-11-22 15:20:00 -0500

    docker-compose and webpacker

---
+++ start.sh
@@ -0,0 +1,13 @@
+
+
+if [ ${APP_ROLE:-app} == "sidekiq" ]; then
+    echo "starting sidekiq"
+    bundle exec sidekiq
+else
+    echo "starting rails"
+    rm -f tmp/pids/server.pid
+    yarn
+    bundle exec rails db:create
+    bundle exec rails db:migrate
+    bundle exec rails s -b '0.0.0.0'
+fi
